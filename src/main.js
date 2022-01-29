620948f6aa6298bb400491da2126bdc18eeab60a
Kuma-inataku <inataku0121@gmail,com>
2022-01-29 22:32:07 +0900

    interceptorsの使用

--- src/main.js
+++ src/main.js
@@ -6,6 +6,31 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-9dcd9/databases/(default)/documents";
 
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    console.log('interceptors request config', config);
+    return config;
+  },
+  error => {
+    console.log('interceptors request error', error);
+    return Promise.reject();
+  }
+);
+const interceptorsResponse = axios.interceptors.response.use(
+  config => {
+    console.log('interceptors response config', config);
+    return config;
+  },
+  error => {
+    console.log('interceptors response error', error);
+    return Promise.reject();
+  }
+);
+
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
