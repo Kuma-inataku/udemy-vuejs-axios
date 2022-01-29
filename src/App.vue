f1be20e06f506481ecec273a357d5c782cde438f
Kuma-inataku <inataku0121@gmail,com>
2022-01-29 20:13:17 +0900

    データの保存

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,49 @@
       id="comment"
       v-model="comment">
     </textarea>
+    <br><br>
+    <button
+      @click="createComment"
+    >送る
+    </button>
     <h2>掲示板</h2>
 
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       name: "",
       comment: "",
+    }
+  },
+  methods: {
+    createComment() {
+      axios.post(
+        'https://firestore.googleapis.com/v1/projects/vuejs-http-9dcd9/databases/(default)/documents/comments',
+        {
+          fields: {
+            name: {
+              stringValue: this.name,
+            },
+            comment: {
+              stringValue: this.comment,
+            }
+          }
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      this.name = "";
+      this.comment = "";
     }
   }
 }
