a364c156be37638f184d36645cafc74fa95104a7
Kuma-inataku <inataku0121@gmail,com>
2022-01-30 11:45:55 +0900

    リダイレクト先指定

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Comments from './views/Comments.vue';
 import Login from './views/Login.vue';
 import Register from './views/Register.vue';
-
+import store from './store';
 Vue.use(Router);
 
 export default new Router({
@@ -12,14 +12,35 @@ export default new Router({
     {
       path: '/',
       component: Comments,
+      beforeEnter(to, from, next) {
+        if (store.getters.idToken) {
+          next();
+        } else {
+          next('login');
+        }
+      }
     },
     {
       path: '/login',
       component: Login,
+      beforeEnter(to, from, next) {
+        if (store.getters.idToken) {
+          next('/');
+        } else {
+          next();
+        }
+      }
     },
     {
       path: '/register',
       component: Register,
+      beforeEnter(to, from, next) {
+        if (store.getters.idToken) {
+          next('/');
+        } else {
+          next();
+        }
+      }
     },
   ]
 })
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from '../axios-auth';
-
+import router from '../router';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -27,6 +27,7 @@ export default new Vuex.Store({
         }
       ).then(response => {
         commit('updateIdToken', response.data.idToken);
+        router.push('/');
       });
     },
     register({ commit }, authData) {
@@ -39,6 +40,7 @@ export default new Vuex.Store({
         }
       ).then(response => {
         commit('updateIdToken', response.data.idToken);
+        router.push('/');
       });
     },
   }
