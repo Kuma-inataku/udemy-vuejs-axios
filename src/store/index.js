b8589001421a27ec13877751bcb3bfc3b3285aaa
Kuma-inataku <inataku0121@gmail,com>
2022-01-30 16:29:19 +0900

    トークンのリフレッシュ処理

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Vuex from "vuex";
 import axios from '../axios-auth';
 import router from '../router';
+import axiosRefresh from '../axios-refresh';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -17,7 +19,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    login({ commit }, authData) {
+    login({ commit, dispatch }, authData) {
       axios.post(
         '/accounts:signInWithPassword?key=AIzaSyAv00TLj_DpntxJ3k2sumCQshxMjnQELJM',
         {
@@ -27,7 +29,24 @@ export default new Vuex.Store({
         }
       ).then(response => {
         commit('updateIdToken', response.data.idToken);
+        setTimeout(() => {
+          dispatch('refreshIdToken', response.data.refreshToken);
+        }, response.data.expiresIn * 1000);
         router.push('/');
+      });
+    },
+    refreshIdToken({ commit, dispatch }, refreshToken) {
+      axiosRefresh.post(
+        '/token?key=AIzaSyAv00TLj_DpntxJ3k2sumCQshxMjnQELJM',
+        {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+        }
+      ).then(response => {
+        commit("updateIdToken", response.data.id_token)
+        setTimeout(() => {
+          dispatch('refreshIdToken', response.data.refresh_token);
+        }, response.data.expires_in * 1000);
       });
     },
     register({ commit }, authData) {
